b06ea711c48fe94bca62d6b43fa33f3c44fe8dfb
shyam <shyamjith38@hotmail.com>
2024-09-07 23:23:26 +0530

    static rendering testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> 
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
