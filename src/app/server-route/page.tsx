24446b4835025424e8142ec756e0718a2f31358b
shyam <shyamjith38@hotmail.com>
2024-09-08 11:56:44 +0530

    removed client-only from server compnt

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,10 +1,8 @@
 import ImageSlider from "@/components/image_slider";
-import { clientSideFunction } from "@/utils/client-utils";
 import { serverSideFunction } from "@/utils/server-utils"
 
 export default function ServerRoutePage(){
     const result =serverSideFunction();
-    const clientResult= clientSideFunction();
     return (<>
     
     <h1>ServerRoutePage</h1>
